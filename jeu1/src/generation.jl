1bf9ddcb6a98d97701dc9813818d0801d090cfa3
brunosanches <brunosanches1@outlook.com>
2022-04-24 09:20:12 +0200

    Create instance generator

--- jeu1/src/generation.jl
+++ jeu1/src/generation.jl
@@ -1,18 +1,96 @@
-# This file contains methods to generate a data set of instances (i.e., sudoku grids)
+# This file contains methods to generate a data set of instances
 include("io.jl")
+using Random
 
 """
 Generate an n*n grid with a given density
 
 Argument
 - n: size of the grid
-- density: percentage in [0, 1] of initial values in the grid
 """
-function generateInstance(n::Int64, density::Float64)
+function generateInstance(n::Int64)
 
-    # TODO
-    println("In file generation.jl, in method generateInstance(), TODO: generate an instance")
-    
+    board = zeros(Int64, n, n)
+
+    function recursiveBoardConstructor(board::Array{Int64, 2}, posi::Int, posj::Int)
+        n = size(board,1)
+        if posi > n || posj > n
+            return true
+        end
+
+        unavailable_values = union(Set(board[posi,:]), Set(board[:, posj]))
+        available_values = setdiff(1:n, unavailable_values)
+        if isempty(available_values)
+            return false
+        end
+
+        shuffle!(available_values)
+        for value in available_values
+            board[posi, posj] = value
+            if posj == n
+                next_posj = 1
+                next_posi = posi+1
+            else
+                next_posj = posj+1
+                next_posi = posi
+            end
+            if recursiveBoardConstructor(board, next_posi, next_posj) == true
+                return true
+            end
+        end
+        board[posi, posj] = 0
+        return false
+    end
+
+    recursiveBoardConstructor(board, 1, 1)
+
+    instance = zeros(Int64, 4, n)
+    # Construct each perspective
+    # Perspective 1
+    for j in 1:n
+        maxelem = 0
+        for i in 1:n
+            if board[i,j] > maxelem
+                instance[1, j] += 1
+                maxelem = board[i,j]
+            end
+        end
+    end
+
+    # Perspective 2
+    for i in 1:n
+        maxelem = 0
+        for j in n:-1:1
+            if board[i,j] > maxelem
+                instance[2, i] += 1
+                maxelem = board[i,j]
+            end
+        end
+    end
+
+    # Perspective 3
+    for j in 1:n
+        maxelem = 0
+        for i in n:-1:1
+            if board[i,j] > maxelem
+                instance[3, j] += 1
+                maxelem = board[i,j]
+            end
+        end
+    end
+
+    # Perspective 4
+    for i in 1:n
+        maxelem = 0
+        for j in 1:n
+            if board[i,j] > maxelem
+                instance[4, i] += 1
+                maxelem = board[i,j]
+            end
+        end
+    end
+
+    return instance
 end 
 
 """
@@ -26,6 +104,3 @@ function generateDataSet()
     println("In file generation.jl, in method generateDataSet(), TODO: generate an instance")
     
 end
-
-
-
