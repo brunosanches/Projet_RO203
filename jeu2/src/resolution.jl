dc24156e2315b8b29b02932f7c064d22c3f78d06
brunosanches <brunosanches1@outlook.com>
2022-04-28 23:53:48 +0200

    Implement cplex solution

--- jeu2/src/resolution.jl
+++ jeu2/src/resolution.jl
@@ -6,26 +6,106 @@ include("generation.jl")
 TOL = 0.00001
 
 """
+Check if the solution gave by CPLEX is connected
+Input: maskGrid - Int Array where element i,j is masked if it equals 1
+"""
+function checkConnectivity(maskGrid::Array{Int64})
+    visitedElements = zeros(Int64, size(maskGrid))
+
+    n = size(maskGrid, 1)
+
+    # Get the first element that is not masked
+    firstElement = findfirst(x -> x == 0, maskGrid)
+
+    function recursiveVisiting(maskGrid::Array{Int64, 2}, visited::Array{Int64, 2}, pos::CartesianIndex)
+        n = size(maskGrid, 1)
+        if !checkbounds(Bool, visited, pos)
+            return
+        elseif visited[pos] == 1
+            return
+        elseif maskGrid[pos] == 1
+            return
+        end
+
+        visited[pos] = 1
+        
+        for (i,j) in [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            newPos = pos + CartesianIndex(i, j)
+            recursiveVisiting(maskGrid, visited, newPos)
+        end
+    end
+
+    recursiveVisiting(maskGrid, visitedElements, firstElement)
+    # Return true if all elements are visited or masked
+    return all(visitedElements .| maskGrid .== 1)
+end
+
+"""
 Solve an instance with CPLEX
 """
-function cplexSolve()
+function cplexSolve(grid::Array{Int64,2})
 
     # Create the model
     m = Model(with_optimizer(CPLEX.Optimizer))
 
     # TODO
     println("In file resolution.jl, in method cplexSolve(), TODO: fix input and output, define the model")
+    n = size(grid,1)
+    vX = zeros(n, n)
 
+    # Correspond aux données du problème en forme binaire
+    valeur = zeros(Int64, n, n, n)
+    for i in 1:n
+        for j in 1:n
+            valeur[i,j,grid[i,j]] = 1
+        end
+    end
+
+    # Variables
+    @variable(m, x[1:n, 1:n], Bin) # 1 si la case est masqué
+
+    # Constraintes
+    # Constraintes de non répetition
+    @constraint(m, [i in 1:n, k in 1:n], sum(valeur[i,j,k]*(1-x[i,j]) for j in 1:n) <= 1)
+    @constraint(m, [j in 1:n, k in 1:n], sum(valeur[i,j,k]*(1-x[i,j]) for i in 1:n) <= 1)
+
+    #@constraint(m, [i in 2:(n-1), j in 2:(n-1)], x[i,j] + x[i, j+1] + x[i, j-1] + x[i+1,j] + x[i-1,j] <= 1)
+
+    @constraint(m, [i in 2:n, j in 1:n], x[i,j] + x[i-1, j] <= 1)
+    @constraint(m, [i in 1:(n-1), j in 1:n], x[i,j] + x[i+1, j] <= 1)
+    @constraint(m, [i in 1:n, j in 2:n], x[i,j] + x[i, j-1] <= 1)
+    @constraint(m, [i in 1:n, j in 1:(n-1)], x[i,j] + x[i, j+1] <= 1)
+
+    @objective(m, Min, sum(x[i,j] for i in 1:n for j in 1:n))
     # Start a chronometer
     start = time()
 
     # Solve the model
     optimize!(m)
 
+    if JuMP.primal_status(m) == JuMP.MathOptInterface.FEASIBLE_POINT
+        vX = Array{Int64}(floor.(JuMP.value.(x)))
+
+        while !checkConnectivity(vX)
+            g = findall(x -> x == 1, vX)
+            @constraint(m, sum(x[idx] for idx in g) <= length(g)-1)
+
+            optimize!(m)
+            if JuMP.primal_status(m) != JuMP.MathOptInterface.FEASIBLE_POINT
+                break
+            end
+            vX = Array{Int64}(floor.(JuMP.value.(x)))
+        end
+
+        displaySolution(grid, vX)
+    end
+
+    
     # Return:
     # 1 - true if an optimum is found
     # 2 - the resolution time
-    return JuMP.primal_status(m) == JuMP.MathOptInterface.FEASIBLE_POINT, time() - start
+    # 3 - Masked tiles
+    return JuMP.primal_status(m) == JuMP.MathOptInterface.FEASIBLE_POINT, time() - start, vX
     
 end
 
